0afd3c3ff869af0d7d9f925b7b4bb2add0d5bfd9
aliyalama@googlemail.com <Gundam1979!>
2021-08-10 16:26:43 +0200

    models wip

--- src/shared/HackerrankJSON/model/HackerrankJSON.ts
+++ src/shared/HackerrankJSON/model/HackerrankJSON.ts
@@ -1,3 +1,37 @@
-export interface HackerrankJSON {
+import {Submission} from "../../Submission/model/Submission";
 
+export interface HackerrankJSON {
+  username: string;
+  email: string;
+  country: string;
+  name: string;
+  website: string;
+  job_title: string;
+  github_url: string;
+  fb_uid: string;
+  gh_uid: string;
+  li_uid: string;
+  linkedin_url: string;
+  personal_first_name: string;
+  personal_last_name: string;
+  timezone: string;
+  is_professional: boolean;
+  hackos: number;
+  favorite_challenges: any[];
+  challenges_created: any[];
+  other_emails: any[];
+  all_schools: any[];
+  snippets: any[];
+  submissions: Submission[];
+  teams: any[];
+  contest_participations: any[];
+  forum_comments: any[];
+  all_ratings: any[];
+  contest_medals: any[];
+  survery_results: any[];
+  messages_sent: any[];
+  run_codes: any[];
+  contests_created: any[];
+  job_applications: any[];
+  skills_tes: any[];
 }
