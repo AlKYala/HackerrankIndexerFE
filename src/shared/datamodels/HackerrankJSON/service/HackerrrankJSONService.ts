af62f9e77049c855b73c28bb62f12079b8aa8d6c
aliyalama@googlemail.com <Gundam1979!>
2021-08-15 13:13:21 +0200

    wip passing parsed data to backend to persist data

---
+++ src/shared/datamodels/HackerrankJSON/service/HackerrrankJSONService.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import {from, Observable} from "rxjs";
+import {HackerrankJSON} from "../model/HackerrankJSON";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HackerrrankJSONService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  private parseHackerrankJSON(hackerrankJsonFile: File)  {
+    from(hackerrankJsonFile.text()).subscribe((data: string) => {
+      const parsed = JSON.parse(data);
+      const hrJSON: HackerrankJSON = {email: parsed.email, username: parsed.username, submissions: parsed.submissions};
+      //TODO send request here
+      this.httpClient.post(`${environment.api}/json`, hrJSON).pipe().subscribe((data) => {
+        console.log(data);
+      });
+    });
+  }
+
+}
