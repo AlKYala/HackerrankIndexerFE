d187b01e63cc869fb3f56af5bdca0b2c11d2c714
aliyalama@googlemail.com <Gundam1979!>
2021-08-17 17:42:35 +0200

    first implementation of basic sidebar and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app.routing-module";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCommonModule} from "@angular/material/core";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataReaderComponent
+    DataReaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCommonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
