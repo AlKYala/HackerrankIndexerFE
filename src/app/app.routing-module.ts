a3b8b2d5ebda02aa060a18a5fc4a46b1fef60838
aliyalama@googlemail.com <Gundam1979!>
2021-08-26 18:35:05 +0200

    wrote submissionDataService and made it obsolete

--- src/app/app.routing-module.ts
+++ src/app/app.routing-module.ts
@@ -3,10 +3,12 @@ import {DataReaderComponent} from "./data-reader/data-reader.component";
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
 import {SubmissionListComponent} from "./submission-list/submission-list.component";
+import {SubmissionDetailComponent} from "./submission-detail/submission-detail.component";
 
 const routes: Routes = [
   {path: 'upload', component: DataReaderComponent},
-  {path: 'submissions', component: SubmissionListComponent}
+  {path: 'submissions', component: SubmissionListComponent},
+  {path: 'submission/:id', component: SubmissionDetailComponent}
 ];
 
 @NgModule({
---
+++ src/app/submission-detail/submission-detail.component.ts
@@ -0,0 +1,86 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Submission} from "../../shared/datamodels/Submission/model/Submission";
+import {formatDate} from "@angular/common";
+import {Subscription} from "rxjs";
+import {SubscriptionService} from "../../shared/services/SubscriptionService";
+import {SubmissionService} from "../../shared/datamodels/Submission/service/SubmissionService";
+import {ActivatedRoute} from "@angular/router";
+import {SubmissionDataService} from "../../shared/services/SubmissionDataService";
+
+@Component({
+  selector: 'app-submission-detail',
+  templateUrl: './submission-detail.component.html',
+  styleUrls: ['./submission-detail.component.css']
+})
+export class SubmissionDetailComponent implements OnInit, OnDestroy {
+
+  submission!: Submission;
+  private subscriptions!: Subscription[];
+  public loaded: boolean = false;
+  private submissionid!: number;
+
+  constructor(private subscriptionService: SubscriptionService,
+              private submissionService: SubmissionService,
+              private submissionDataService: SubmissionDataService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.subscriptions = [];
+    this.fetchSubmission();
+    this.resolveSubmissionId();
+  }
+
+  ngOnDestroy() {
+    this.subscriptionService.unsubscribeParam(this.subscriptions);
+  }
+
+  private resolveSubmissionId(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(typeof id === "string") {
+      this.submissionid = parseInt(id);
+    }
+  }
+
+  private fetchSubmission() {
+    this.submissionService.findById(this.submissionid).pipe().subscribe((submission: Submission) => {
+      this.submission = submission;
+      this.loaded;
+    });
+  }
+
+  public generateAndDownloadSubmission() {
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    const blob = new Blob([this.generateFileContents(this.submission)], {type: "octet/stream"});
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    const fileName: string = `${this.submission.challenge.challengeName.replace(/\s/g, "")}.java`;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  private generateFileContents(submission: Submission): string {
+    return `${this.generateDocumentInfo(submission)}\n${submission.code}`;
+  }
+
+  private generateDocumentInfo(submission: Submission): string {
+    return `/**\nPowered by HackerrankIndexer by Ali Yalama 2021\n
+    https://github.com/AlKYala/HackerRankIndexer\n
+    File created: ${this.getCurrentDateAsString()}\n
+    Challenge name: ${submission.challenge.challengeName}\n
+    Author: ${submission.writer.username}\n*/\n`;
+  }
+
+  private getCurrentDateAsString(): string {
+    return formatDate(new Date(), "dd/MM/yyyy", 'en');
+  }
+
+  /*private getSubmission(id: number): void {
+    const subscription: Subscription = this.submissionService.findById(id).pipe().subscribe((submission: Submission) => {
+      this.submission = submission;
+    });
+    this.subscriptions.push(subscription);
+  }*/
+}
