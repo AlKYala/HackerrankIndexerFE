c6c6389c30b9050ae9770c22c413133e92b4ec87
aliyalama@googlemail.com <Gundam1979!>
2021-10-02 17:44:28 +0200

    pagination functionality fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app.routing-module";
-import { NavbarComponent } from './navbar/navbar.component';
-import { SubmissionListComponent } from './submission-list/submission-list.component';
-import { SubmissionDetailComponent } from './submission-detail/submission-detail.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SubmissionListComponent} from './submission-list/submission-list.component';
+import {SubmissionDetailComponent} from './submission-detail/submission-detail.component';
+import {AnalyticsComponent} from './analytics/analytics.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {NgxChartModule} from "ngx-chart";
 import {PieChartModule} from "@swimlane/ngx-charts";
-import { GeneralstatscomponentComponent } from './generalstatscomponent/generalstatscomponent.component';
-import { LanguageSubmissionPassRateComponentComponent } from './language-submission-pass-rate-component/language-submission-pass-rate-component.component';
-import { ChartcomponentComponent } from './chartcomponent/chartcomponent.component';
+import {GeneralstatscomponentComponent} from './generalstatscomponent/generalstatscomponent.component';
+import {LanguageSubmissionPassRateComponentComponent} from './language-submission-pass-rate-component/language-submission-pass-rate-component.component';
+import {ChartcomponentComponent} from './chartcomponent/chartcomponent.component';
 import {NgxPaginationModule} from "ngx-pagination";
-import { LandingComponent } from './landing/landing.component';
-import { HowtocomponentComponent } from './howtocomponent/howtocomponent.component';
-import { IntroFeaturesComponentComponent } from './intro-features-component/intro-features-component.component';
+import {LandingComponent} from './landing/landing.component';
+import {HowtocomponentComponent} from './howtocomponent/howtocomponent.component';
+import {IntroFeaturesComponentComponent} from './intro-features-component/intro-features-component.component';
+import {JwPaginationModule} from "jw-angular-pagination";
 
 @NgModule({
   declarations: [
@@ -34,18 +35,20 @@ import { IntroFeaturesComponentComponent } from './intro-features-component/intr
     HowtocomponentComponent,
     IntroFeaturesComponentComponent,
   ],
-    imports: [
-        RouterModule,
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        FontAwesomeModule,
-        NgxChartModule,
-        PieChartModule,
-        NgxPaginationModule
-    ],
+  imports: [
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgxChartModule,
+    PieChartModule,
+    NgxPaginationModule,
+    JwPaginationModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
