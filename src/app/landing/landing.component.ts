9dae0d125dd9af58abb161a7fb1b7098948bf62c
aliyalama@googlemail.com <Gundam1979!>
2022-01-17 15:15:19 +0100

    landing site wip

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LocalStorageService} from "ngx-webstorage";
+import {Subscription} from "rxjs";
+import {AnalyticsService} from "../../shared/services/AnalyticsService";
 
 @Component({
   selector: 'app-landing',
@@ -8,12 +10,37 @@ import {LocalStorageService} from "ngx-webstorage";
 })
 export class LandingComponent implements OnInit {
 
-  loggedIn: boolean = false;
+  loggedIn:             boolean;
+  numberOfUsers!:       number;
+  numberOfSubmissions!: number;
+  subscriptions:        Subscription;
 
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService,
+              private analyticsService: AnalyticsService) {
+    this.loggedIn = false;
+    this.subscriptions = new Subscription();
+  }
 
   ngOnInit(): void {
-    //TODO check for login
+    //TODO check for login - if logged in redirekt to user Interface
+    this.initNumberOfUsers();
+    this.initNumberOfSubmissions();
+  }
+
+  private initNumberOfUsers() {
+    const subscription = this.analyticsService.getNumberOfUsers().pipe()
+      .subscribe((data: number) => {
+      //this.numberOfUsers = data;
+        this.numberOfUsers = data;
+    });
+    this.subscriptions.add(subscription);
+  }
+
+  private initNumberOfSubmissions() {
+    const subscription = this.analyticsService.getNumberOfSubmissions().pipe()
+      .subscribe((data: number) => {
+        this.numberOfSubmissions = data;
+      })
   }
 
 }
