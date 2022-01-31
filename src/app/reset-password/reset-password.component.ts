a72e921288052c2ec20f7a1e2f055ef5a3ca77bd
aliyalama@googlemail.com <Gundam1979!>
2022-01-31 11:58:30 +0100

    wip trigger password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {FormcontrolsSettings} from "../../shared/other/formcontrols.settings";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-reset-password',
@@ -13,13 +17,21 @@ export class ResetPasswordComponent implements OnInit {
 
   public emailForm!: FormControl;
 
-  constructor() { }
+  private baseSubscription = new Subscription();
+
+  private token!: string;
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.initLoginForm();
   }
 
   //TODO link to view
   public initLoginForm(): void {
+
+    //this.grabQueryParam();
+
     this.resetForm = new FormGroup({});
 
     this.emailForm = FormcontrolsSettings.emailFormControl();
@@ -27,4 +39,17 @@ export class ResetPasswordComponent implements OnInit {
     this.resetForm.addControl("loginEmail", this.emailForm);
   }
 
+  public triggerPasswordReset() {
+    const subscription = this.httpClient.post(`${environment.api}/resetPassword`, this.emailForm.value).subscribe();
+    console.log(this.emailForm.value);
+    this.baseSubscription.add(subscription);
+  }
+
+  //TODO does not belong in this component
+  private grabQueryParam() {
+    const subscription: Subscription = this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+    });
+    this.baseSubscription.add(subscription)
+  }
 }
