d3ca6cf1cb4920f5a940e5b37b8ac37157eab4a1
aliyalama@googlemail.com <Gundam1979!>
2022-02-01 20:48:02 +0100

    added RoutingService.ts
    Redirect from landing to analytics if logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {MediaMatcher} from "@angular/cdk/layout";
 import {Router} from "@angular/router";
+import {RoutingService} from "../../shared/services/RoutingService";
 
 @Component({
   selector: 'app-navbar',
@@ -11,17 +12,23 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   public toggle: boolean;
 
+  public username!: string;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private routerService: RoutingService) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.toggle = false;
+  }
+
   ngOnInit(): void {
   }
   mobileQuery: MediaQueryList;
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-    this.toggle = false;
+  private initUsername() {
+
   }
 
   public toggleDropdown() {
@@ -34,7 +41,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   public route(event: Event, path: string) {
-    event.preventDefault();
-    this.router.navigate([path]);
+    this.routerService.route(path, event);
   }
 }
